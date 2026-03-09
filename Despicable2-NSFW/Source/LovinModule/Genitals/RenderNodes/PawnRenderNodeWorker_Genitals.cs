40bd39aa57ad4e55d68551fa71d2d06e33145696
Deavy <deavy.jslzr@gmail.com>
2026-03-08 19:29:12 -0700

    Fixed null reference errors for anim studio

--- Despicable2-NSFW/Source/LovinModule/Genitals/RenderNodes/PawnRenderNodeWorker_Genitals.cs
+++ Despicable2-NSFW/Source/LovinModule/Genitals/RenderNodes/PawnRenderNodeWorker_Genitals.cs
@@ -1,60 +1,86 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using RimWorld;
+
 using UnityEngine;
+
 using Verse;
 
 namespace Despicable;
+
 public class PawnRenderNodeWorker_Genitals : PawnRenderNodeWorker_FlipWhenCrawling
 {
     public override bool CanDrawNow(PawnRenderNode node, PawnDrawParms parms)
     {
-        if (base.CanDrawNow(node, parms))
+        if (!base.CanDrawNow(node, parms))
         {
-            if (!(parms.facing != Rot4.North))
-            {
-                return parms.flipHead;
-            }
+            return false;
+        }
+
+        if (parms.facing != Rot4.North)
+        {
             return true;
         }
-        return false;
+
+        return parms.flipHead;
     }
 
     public override Quaternion RotationFor(PawnRenderNode node, PawnDrawParms parms)
     {
-        AnimationDef animationDef = parms.pawn.Drawer.renderer.renderTree.currentAnimation;
-        AnimationPart animPart;
-        node.tree.TryGetAnimationPartForNode(node, out animPart);
+        AnimationDef animationDef = parms.pawn?.Drawer?.renderer?.renderTree?.currentAnimation;
 
-        float num = node.DebugAngleOffset;
-        if (node.Props.drawData != null)
+        AnimationPart animPart = null;
+        bool hasAnimPart = node?.tree != null && node.tree.TryGetAnimationPartForNode(node, out animPart);
+
+        float num = node?.DebugAngleOffset ?? 0f;
+
+        if (node?.parent?.Props?.drawData != null)
         {
             num += node.parent.Props.drawData.RotationOffsetForRot(parms.facing);
         }
-        if (node.parent.AnimationWorker != null && node.parent.AnimationWorker.Enabled(animationDef, node, animPart, parms) && !parms.flags.FlagSet(PawnRenderFlags.Portrait))
+
+        bool canUseAnimationAngle =
+            node?.parent?.AnimationWorker != null &&
+            animationDef != null &&
+            hasAnimPart &&
+            animPart != null &&
+            !WorkshopRenderContext.Active &&
+            !parms.flags.FlagSet(PawnRenderFlags.Portrait) &&
+            node.parent.AnimationWorker.Enabled(animationDef, node, animPart, parms);
+
+        if (canUseAnimationAngle)
         {
-            num += node.parent.AnimationWorker.AngleAtTick(node.tree.AnimationTick, animationDef, node, animPart, parms);
+            num += node.parent.AnimationWorker.AngleAtTick(
+                node.tree.AnimationTick,
+                animationDef,
+                node,
+                animPart,
+                parms
+            );
         }
-        if (node.hediff?.Part?.flipGraphic ?? false)
+
+        if (node?.hediff?.Part?.flipGraphic ?? false)
         {
             num *= -1f;
         }
+
         return Quaternion.AngleAxis(num, Vector3.up);
     }
 
     public override Vector3 OffsetFor(PawnRenderNode node, PawnDrawParms parms, out Vector3 pivot)
     {
         Vector3 vector = base.OffsetFor(node, parms, out pivot);
-        string bodyType = parms.pawn.story.bodyType.defName;
-        BodyTypeGenitalsOffsetDef bodyTypeAppendageOffsetDef = DefDatabase<BodyTypeGenitalsOffsetDef>.GetNamedSilentFail(bodyType);
-        Vector3? bodyTypeAppendageOffset = null;
-        if (bodyTypeAppendageOffsetDef != null)
-            bodyTypeAppendageOffset = bodyTypeAppendageOffsetDef.offset;
 
-        if (bodyTypeAppendageOffset != null )
+        string bodyType = parms.pawn?.story?.bodyType?.defName;
+        if (string.IsNullOrEmpty(bodyType))
+        {
+            return vector;
+        }
+
+        BodyTypeGenitalsOffsetDef bodyTypeAppendageOffsetDef =
+            DefDatabase<BodyTypeGenitalsOffsetDef>.GetNamedSilentFail(bodyType);
+
+        Vector3? bodyTypeAppendageOffset = bodyTypeAppendageOffsetDef?.offset;
+
+        if (bodyTypeAppendageOffset != null)
         {
             if (parms.facing == Rot4.East)
             {
@@ -65,8 +91,8 @@ public class PawnRenderNodeWorker_Genitals : PawnRenderNodeWorker_FlipWhenCrawli
                 vector += Vector3.left * bodyTypeAppendageOffset.Value.x;
             }
 
-            vector += new Vector3(0, 1, 0) * bodyTypeAppendageOffset.Value.y;
-            vector += new Vector3(0, 0, 1) * bodyTypeAppendageOffset.Value.z;
+            vector += Vector3.up * bodyTypeAppendageOffset.Value.y;
+            vector += Vector3.forward * bodyTypeAppendageOffset.Value.z;
         }
 
         return vector;
